5f5869126181bce33584442d315d1013b47a671f
pug3eye <pug3eye@gmail.com>
2017-09-07 00:47:13 +0700

    switch between states

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component, trigger, state, style } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  animations: [
+    trigger('divState', [
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0)'
+      })),
+      state('highlighted', style({
+        backgroundColor: 'blue',
+        transform: 'translateX(100px)'
+      }))
+    ])
+  ]
+})
+export class AppComponent {
+  state = 'normal';
+  list = ['Milk', 'Sugar', 'Bread'];
+
+  onAdd(item) {
+    this.list.push(item);
+  }
+
+  onDelete(item) {
+    this.list.splice(this.list.indexOf(item), 1);
+  }
+ 
+}
